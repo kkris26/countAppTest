7cac113646bc757d3b7f71732a2a1ef6febe43a7
kkris26 <krisnuartha09@gmail.com>
2025-02-21 07:44:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0)
+const addToCount = () =>{
+  count.value = count.value+1
+
+}
+const subToCount =() =>{
+  count.value = count.value-1
+}
+
+
+</script>
+
+<template>
+  <main>
+
+    <div>
+      <h1 id = 'number'>{{count}}</h1>
+      <button @click="addToCount()">+</button>
+      <button @click="subToCount">-</button>
+    </div>
+  </main>
+</template>
+
+
+
+<style scoped>
+main{
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#number {
+  font-size: 70px;
+  text-align: center;
+}
+
+button{
+  padding: 10px 20px;
+}
+
+</style>
